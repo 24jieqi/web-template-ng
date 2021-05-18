ae0e0aa3814b57fad5141366dd217b206044291b
winljm001 <819648983@qq.com>
2021-05-18 19:39:36 +0800

    fix: 格式化

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import { RouteConfigComponentProps, renderRoutes } from 'react-router-config'
-
+import React from 'react';
+import { RouteConfigComponentProps, renderRoutes } from 'react-router-config';
 
 const Layout: React.FC<RouteConfigComponentProps> = React.memo((props) => {
-  const { route } = props
-  return renderRoutes(route?.routes)
-})
-export default Layout
+  const { route } = props;
+  return renderRoutes(route?.routes);
+});
+export default Layout;
--- src/router/config/index.tsx
+++ src/router/config/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
-const Home:React.FC = () => {
-  return <div>
-    <h1>1</h1>
-  </div>
-}
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>1</h1>
+    </div>
+  );
+};
 const routes = [
   {
-    path: "/",
+    path: '/',
     exact: true,
-    component: Home
-  }
-]
-
+    component: Home,
+  },
+];
 
 export default routes;
